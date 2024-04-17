7db2cb70a86de51bcbea1536be2dc0cda1d9ae36
didoslavov <didkoslawow@gmail.com>
2024-04-18 01:48:55 +0300

    use flex in main

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Actions from './Actions';
 
 function Header() {
     return (
-        <header className="flex items-center justify-between w-[80%] text-dark-blue mt-8 m-auto py-4">
+        <header className="flex items-center justify-between w-[80%] text-dark-blue mt-8 py-4">
             <div className="text-[29.65px] leading-[140%] -tracking-[0.62px] font-bold font-grotesk">Website</div>
             <Nav />
             <Actions />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import MainHeading from './components/MainHeading';
 
 export default function Home() {
     return (
-        <main className="">
+        <main className="flex flex-col items-center">
             <Header />
+            <MainHeading />
         </main>
     );
 }
