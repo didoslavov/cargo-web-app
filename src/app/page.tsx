84493f4ffba02ba1029a74582b5ddf53f96bb897
didoslavov <didkoslawow@gmail.com>
2024-04-18 01:55:32 +0300

    add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image';
 import Header from './components/Header/Header';
 import MainHeading from './components/MainHeading';
+import truck from '/public/truck.png';
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center">
             <Header />
             <MainHeading />
+            <Image src={truck} width={969} height={690} alt="Image of truck parked on a ramp" className="mt-[64px]" />
         </main>
     );
 }
