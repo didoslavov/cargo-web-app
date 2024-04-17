47e68ce4d579c5c36ed1696437c954eacb431504
didoslavov <didkoslawow@gmail.com>
2024-04-18 01:36:27 +0300

    add custom colors and font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                'main-white': '#F5F5F5',
+                'dark-blue': {
+                    DEFAULT: '#01303A',
+                    800: '#3C4A47',
+                    900: '#083C2F',
+                },
+            },
+        },
+        fontFamily: {
+            grotesk: ['Space Grotesk', 'sans-serif'],
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
