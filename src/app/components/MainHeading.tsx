45a71c0f686ec325535c45ddf9c77d0316589d3c
didoslavov <didkoslawow@gmail.com>
2024-04-18 01:49:01 +0300

    add main heading

---
+++ src/app/components/MainHeading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainHeading() {
+    return (
+        <h1 className="font-grotesk font-semibold text-7xl leading-[108%] -tracking-[4px] text-dark-blue w-[50%] text-center mt-[72px]">
+            Гъвкави решения за сухопътен транспорт за всеки клиент
+        </h1>
+    );
+}
+
+export default MainHeading;
