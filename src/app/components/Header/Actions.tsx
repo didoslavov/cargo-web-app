04929f15ccfd70a824ebd921132d014d96fe2485
didoslavov <didkoslawow@gmail.com>
2024-04-18 01:33:37 +0300

    add cta button and link

---
+++ src/app/components/Header/Actions.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Actions() {
+    return (
+        <div className="text-[16px] tracking-[0px] leading-[170%] flex gap-9 items-center">
+            <a href="#" className="font-medium">
+                Профил
+            </a>
+            <button className="font-semibold border-[2px] border-dark-blue text-dark-blue rounded-lg py-[12px] px-[14px]">
+                Заявка за пратка
+            </button>
+        </div>
+    );
+}
+
+export default Actions;
