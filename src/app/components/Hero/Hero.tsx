ec9cca18f75fad3e68f2abcddc3f7695cadbd0e9
didoslavov <didkoslawow@gmail.com>
2024-04-18 04:25:47 +0300

    remove unnecessary import

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import MainHeading from '../MainHeading';
 import Image from 'next/image';
 import truck from '/public/truck.png';
 
 function Hero() {
     return (
         <>
-            <MainHeading />
+            <h1 className="font-grotesk font-semibold text-7xl leading-[108%] -tracking-[4px] text-dark-blue w-[50%] text-center mt-[72px]">
+                Гъвкави решения за сухопътен транспорт за всеки клиент
+            </h1>
             <Image src={truck} width={969} height={690} alt="Камион, паркиран на рампа за товарене" className="mt-[64px]" />
         </>
     );
