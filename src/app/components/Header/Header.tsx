2e43378db9764d3acb6f9da467de63418ea5bc77
didoslavov <didkoslawow@gmail.com>
2024-04-18 13:23:48 +0300

    add mobile menu button

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
 import Actions from './Actions';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 function Header() {
     return (
-        <header className="flex items-center justify-between w-[80%] text-dark-blue mt-8 py-4">
+        <header className="flex items-center justify-between w-[90%] xl:w-[80%] text-dark-blue mt-8 py-4">
             <div className="text-[29.65px] leading-[140%] -tracking-[0.62px] font-bold font-grotesk">Website</div>
             <Nav />
-            <Actions />
+            <RxHamburgerMenu className="text-2xl lg:hidden" />
         </header>
     );
 }
