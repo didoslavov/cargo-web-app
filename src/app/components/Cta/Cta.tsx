a4089af0b9c288117a4c4661674ce674381f7737
didoslavov <didkoslawow@gmail.com>
2024-04-18 13:22:48 +0300

    add responsive styles

--- src/app/components/Cta/Cta.tsx
+++ src/app/components/Cta/Cta.tsx
@@ -5,11 +5,11 @@ import Form from './Form';
 function Cta() {
     return (
         <section className="flex justify-center mt-[100px] bg-dark-blue-700 w-full text-center py-[100px]">
-            <article className="flex flex-col items-center w-[40%]">
-                <h2 className="font-grotesk text-[72px] font-semibold leading-[108%] -tracking-[4px] text-white">
+            <article className="flex flex-col items-center xl:w-[40%]">
+                <h2 className="font-grotesk  2xl:text-7xl font-semibold md:leading-[108%] md:-tracking-[4px] text-white md:mt-[72px] xl:text-6xl lg:text-5xl sm:text-4xl text-xl">
                     Готово ли сте да работим заедно?
                 </h2>
-                <p className="mt-[24px] leading-[170%] -tracking-[0] text-white text-[20px] w-[80%]">
+                <p className="mt-[24px] leading-[170%] -tracking-[0] text-white md:text-[20px] w-[80%]">
                     Оставете вашият телефон и ние ще се свържем с вас, за да отговорим на всички въпроси
                 </p>
                 <Form />
