82f5bf8f6b675bef1099c60e18207cc7dd10810f
didoslavov <didkoslawow@gmail.com>
2024-04-18 13:24:24 +0300

    add responsive styles

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -5,10 +5,16 @@ import truck from '/public/truck.png';
 function Hero() {
     return (
         <>
-            <h1 className="font-grotesk font-semibold text-7xl leading-[108%] -tracking-[4px] text-dark-blue w-[50%] text-center mt-[72px]">
+            <h1 className="font-grotesk font-semibold 2xl:text-7xl md:leading-[108%] md:-tracking-[4px] text-dark-blue w-[50%] text-center mt-[24px] md:mt-[72px] xl:text-6xl lg:text-5xl sm:text-4xl text-xl">
                 Гъвкави решения за сухопътен транспорт за всеки клиент
             </h1>
-            <Image src={truck} width={969} height={690} alt="Камион, паркиран на рампа за товарене" className="mt-[64px]" />
+            <Image
+                src={truck}
+                width={969}
+                height={690}
+                alt="Камион, паркиран на рампа за товарене"
+                className="mt-[64px] max-w-[80%]"
+            />
         </>
     );
 }
