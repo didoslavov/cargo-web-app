ffe231ef52cba152702359d57de1cec30699f953
didoslavov <didkoslawow@gmail.com>
2024-04-18 13:33:33 +0300

    add mobile menu button and funcitonality

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Nav from './Nav';
-import Actions from './Actions';
 import { RxHamburgerMenu } from 'react-icons/rx';
 
 function Header() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobile = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <header className="flex items-center justify-between w-[90%] xl:w-[80%] text-dark-blue mt-8 py-4">
             <div className="text-[29.65px] leading-[140%] -tracking-[0.62px] font-bold font-grotesk">Website</div>
-            <Nav />
-            <RxHamburgerMenu className="text-2xl lg:hidden" />
+            <Nav isMobileMenuOpen={isMobileMenuOpen} toggleMobile={toggleMobile} />
+            <RxHamburgerMenu onClick={toggleMobile} className="text-4xl lg:hidden cursor-pointer" />
         </header>
     );
 }
--- src/app/components/Header/Nav.tsx
+++ src/app/components/Header/Nav.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import { IoMdClose } from 'react-icons/io';
+import React, { useState } from 'react';
 import NavItem from './NavItem';
 import Actions from './Actions';
 
-function Nav() {
+function Nav({ isMobileMenuOpen, toggleMobile }: { isMobileMenuOpen: boolean; toggleMobile: () => void }) {
     const navItems = ['Начало', 'За нас', 'Услуги', 'Цени', 'Как работи', 'Контакти'];
 
     return (
         <>
-            <nav className="flex gap-14 xl:gap-44">
-                <ul className="hidden lg:flex items-center lg:gap-[40px] font-medium text-dark-blue-800 text-[16px] tracking-[0px] leading-[170%]">
-                    {navItems.map((item, i) => (
-                        <NavItem key={i}>{item}</NavItem>
-                    ))}
-                </ul>
-                <Actions className="hidden lg:flex lg:gap-9 lg:items-center" />
-            </nav>
-            <div className="flex flex-col gap-16 lg:hidden justify-center items-center w-screen h-screen bg-main-white  fixed top-0 right-0 z-50">
-                <ul className="flex flex-col gap-6 font-bold">
-                    {navItems.map((item, i) => (
-                        <NavItem key={i}>{item}</NavItem>
-                    ))}
-                </ul>
-                <Actions className="flex flex-col w-[30%] items-center gap-3" />
-            </div>
+            {!isMobileMenuOpen ? (
+                <nav className="flex gap-14 xl:gap-44">
+                    <ul className="hidden lg:flex items-center lg:gap-[40px] font-medium text-dark-blue-800 text-[16px] tracking-[0px] leading-[170%]">
+                        {navItems.map((item, i) => (
+                            <NavItem key={i}>{item}</NavItem>
+                        ))}
+                    </ul>
+                    <Actions className="hidden lg:flex lg:gap-9 lg:items-center" />
+                </nav>
+            ) : (
+                <div className="flex flex-col gap-16 lg:hidden justify-center items-center w-screen h-screen bg-main-white  fixed top-0 right-0 z-50">
+                    <IoMdClose className="text-4xl absolute top-14 right-8 cursor-pointer" onClick={toggleMobile} />
+                    <ul className="flex flex-col gap-6 font-bold">
+                        {navItems.map((item, i) => (
+                            <NavItem key={i}>{item}</NavItem>
+                        ))}
+                    </ul>
+                    <Actions className="flex flex-col w-[30%] items-center gap-3" />
+                </div>
+            )}
         </>
     );
 }
