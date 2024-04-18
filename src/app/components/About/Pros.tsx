888d055deb08e8fcab162a3c3cd53b133302893f
didoslavov <didkoslawow@gmail.com>
2024-04-18 13:22:31 +0300

    fix styles in md break point

--- src/app/components/About/Pros.tsx
+++ src/app/components/About/Pros.tsx
@@ -2,8 +2,8 @@ import Summary from './Summary';
 
 function Pros() {
     return (
-        <article className="mt-[64px] text-[18px] leading-[170%] tracking-[0px]">
-            <h2 className="font-grotesk text-[48px] leading-[115%] -tracking-[2px] font-semibold">Предимства</h2>
+        <article className="mt-[64px] text-xs md:text-[18px] leading-[170%] tracking-[0px]">
+            <h2 className="font-grotesk text-4xl md:text-[48px] leading-[115%] -tracking-[2px] font-semibold">Предимства</h2>
             <Summary />
         </article>
     );
