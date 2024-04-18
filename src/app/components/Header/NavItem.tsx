675cda0bde21126344a80118db1088f9ee164118
didoslavov <didkoslawow@gmail.com>
2024-04-18 13:24:01 +0300

    add hover effect

--- src/app/components/Header/NavItem.tsx
+++ src/app/components/Header/NavItem.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react';
 function NavItem({ children }: { children: ReactNode }) {
     return (
         <li>
-            <a href="#" className="text-dark-blue-900">
+            <a href="#" className="text-dark-blue-900 hover:text-dark-blue-400 hover:underline transition-all">
                 {children}
             </a>
         </li>
