6085ca3ea6c58d7ddd43032ed0b5024ae1b7156a
didoslavov <didkoslawow@gmail.com>
2024-04-18 13:50:16 +0300

    add hover effects

--- src/app/components/Cta/Form.tsx
+++ src/app/components/Cta/Form.tsx
@@ -9,12 +9,17 @@ function Form() {
                     <BiEnvelope width={16} height={16} />
                     <span className="text-[16px] leading-[24px] -tracking-[0.3px] ">Мобилен телефон</span>
                 </div>
-                <input type="text" name="phone" id="phone" className="py-3 md:py-[24px] px-4 md:px-[20px] rounded-l-md w-[40%]" />
+                <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    className="outline-none focus:outline-cta-green py-3 md:py-[24px] px-4 md:px-[20px] rounded-l-md w-[40%]"
+                />
             </label>
             <input
                 type="submit"
                 value="Изпрати"
-                className="py-3 md:py-[24px] px-4 md:px-[36px] font-semibold text-[16px] leading-[24px] -tracking-[0.3px] text-dark-blue-900 bg-cta-green rounded-r-md"
+                className="hover:cursor-pointer py-3 md:py-[24px] px-4 md:px-[36px] font-semibold text-[16px] leading-[24px] -tracking-[0.3px] text-dark-blue-900 bg-cta-green rounded-r-md"
             />
         </form>
     );
--- src/app/components/Header/Actions.tsx
+++ src/app/components/Header/Actions.tsx
@@ -1,10 +1,10 @@
 function Actions({ className }: { className: string }) {
     return (
         <div className={`text-[16px] tracking-[0px] leading-[170%] ${className}`}>
-            <a href="#" className="font-medium">
+            <a href="#" className="font-medium hover:text-dark-blue-400 hover:underline transition-all">
                 Профил
             </a>
-            <button className="font-semibold border-[2px] border-dark-blue text-dark-blue rounded-lg py-[12px] px-[14px]">
+            <button className="font-semibold border-[2px] hover:text-white hover:bg-dark-blue-400 border-dark-blue text-dark-blue rounded-lg py-[12px] px-[14px]">
                 Заявка за пратка
             </button>
         </div>
