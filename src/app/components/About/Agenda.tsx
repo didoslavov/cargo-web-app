9268b837f1e7b8a57da3d38adf3216eee5c2d518
didoslavov <didkoslawow@gmail.com>
2024-04-18 04:24:52 +0300

    extract summary in component

--- src/app/components/About/Agenda.tsx
+++ src/app/components/About/Agenda.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
+import Summary from './Summary';
 
 function Agenda() {
     return (
         <article className="text-[18px] leading-[170%] tracking-[0px]">
             <h2 className="font-grotesk text-[48px] leading-[115%] -tracking-[2px] font-semibold mt-[64px]">Решения</h2>
-            <p className="mt-[40px]">
-                Фокусът ни върху клиентите е същността на нашата работа. Ние доставяме услуги за всички видове бизнес – от малки
-                местни предприятия до мултинационални компании.
-            </p>
-            <p className="mt-[40px]">
-                Ние обработваме вашата пратка като всяка друга голяма товарна пратка – старателно и професионално.
-            </p>
+            <Summary />
             <ul className="list-disc list-inside">
                 <li>Решения за всички видове стоки (конвенционални, изискващи температурен режим, опасни товари)</li>
                 <li>Превоз на стоки от и до Балканите и всички страни на ЕС, цели и частични товари;</li>
--- src/app/components/About/Pros.tsx
+++ src/app/components/About/Pros.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
+import Summary from './Summary';
 
 function Pros() {
     return (
         <article className="mt-[64px] text-[18px] leading-[170%] tracking-[0px]">
             <h2 className="font-grotesk text-[48px] leading-[115%] -tracking-[2px] font-semibold">Предимства</h2>
-            <p className="mt-[40px]">
-                Фокусът ни върху клиентите е същността на нашата работа. Ние доставяме услуги за всички видове бизнес – от малки
-                местни предприятия до мултинационални компании.
-            </p>
-            <p className="mt-[40px]">
-                Ние обработваме вашата пратка като всяка друга голяма товарна пратка – старателно и професионално.
-            </p>
+            <Summary />
         </article>
     );
 }
